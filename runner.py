7d9faa2ee7d05fe039e146b2c52a070153200904
andriibuimistr <andriibuimistr@gmail.com>
2022-12-25 20:13:58 +0200

    Add requests

--- runner.py
+++ runner.py
@@ -1,8 +1,11 @@
 import pytest
 import allure
+import requests
 
 
 def test1_fail():
+    r = requests.request("Get", "http://127.0.0.1:5000/")
+    allure.attach("name", r.text)
     with pytest.allure.step('FAILED'):
         assert True
 
